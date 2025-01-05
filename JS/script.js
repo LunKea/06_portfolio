d0a6b6716fb16f434bd97daa460fc003f7e81e4d
LunKEA2024 <maaq0001@stud.kea.dk>
2025-01-05 07:46:12 +0100

    Har lavet javascript function til at gemme portfolio når man loader siden, vise portfolio sectionen når man klikker på knappen, og gemmer det igen og går i toppen når man klikker på knappen i bunden

--- JS/script.js
+++ JS/script.js
@@ -1,11 +1,51 @@
 
 // Burger menu
-document.querySelector(".burger-menu").addEventListener("click", function () {
-    // document.querySelector(".nav-list").classList.toggle("show");
-    document.querySelector(".overlay").style.display = "block";
-  });
+// document.querySelector(".burger-menu").addEventListener("click", function () {
+//     // document.querySelector(".nav-list").classList.toggle("show");
+//     document.querySelector(".overlay").style.display = "block";
+//   });
   
-  document.querySelector(".overlay").addEventListener("click", function () {
-    // document.querySelector(".nav-list").classList.remove("show");
-    document.querySelector(".overlay").style.display = "none";
-  });
+//   document.querySelector(".overlay").addEventListener("click", function () {
+//     // document.querySelector(".nav-list").classList.remove("show");
+//     document.querySelector(".overlay").style.display = "none";
+//   });
+
+
+  // mobile portfolio click-scroll
+  // Scroll to section function
+  
+
+    // Gemmer Portfolio sektionerne når siden loades
+    window.addEventListener("load", gemPortfolio);
+      
+    function gemPortfolio(){
+    console.log("Gemmer portfolio sektionerne");
+    document.querySelector(".portfolio_section").classList.add("hide");
+    }
+
+    // Vis portfolio baseret på hvilken knap man trykker på
+    document.querySelector(".intro_btn").addEventListener("click", showIntroWeek_en);
+
+    function showIntroWeek_en(){
+      console.log("Showing intro week EN");
+      document.querySelector(".introweek_en").classList.remove("hide");
+      window.scrollTo({
+        top: 2400,
+        left: 0,
+        behavior: "smooth",
+      });
+  
+    }
+
+
+    // Back to top button
+    document.querySelector(".backToTop").addEventListener("click", ToTopFunction);
+    function ToTopFunction(){
+      console.log("Går tilbage til toppen")
+      window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: "smooth",
+      });
+      setTimeout(gemPortfolio, 700)
+    }
